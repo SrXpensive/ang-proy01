6d48c863e447a71dd23c620449019ad940171c8c
SrXpensive <eduardo.caro.lorente@gmail.com>
2023-07-20 18:12:33 +0200

    Fin tema 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular proy01';
+  title = 'Angular Products';
   author= 'Edu Caro'
 }
