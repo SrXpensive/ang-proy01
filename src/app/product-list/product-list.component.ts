c5702946ad861c5a03aafe469e9667ad0bac81be
SrXpensive <eduardo.caro.lorente@gmail.com>
2023-07-21 20:04:19 +0200

    Pipes

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -9,7 +9,9 @@ import { IProduct } from '../interfaces/i-product';
 })
 export class ProductListComponent {
   title = "Mi lista de productos";
-  headers = { desc: 'Producto', price: 'Precio', avail: 'Disponible' };
+  headers = { desc: 'Producto', price: 'Precio', avail: 'Disponible', image: 'Imagen' };
+  showImage = true;
+  filterSearch: string = '';
   products: IProduct[] = [{
     id: 1,
     desc: 'SSD hard drive',
@@ -17,12 +19,16 @@ export class ProductListComponent {
     price: 75,
     imageUrl: 'assets/ssd.jpg',
     rating: 5
-    },{
+  }, {
     id: 2,
     desc: 'LGA1151 Motherboard',
     avail: new Date('2016-09-15'),
     price: 96.95,
     imageUrl: 'assets/motherboard.jpg',
     rating: 4
-    }];
+  }];
+
+  toggleImage() {
+    this.showImage = !this.showImage;
+  }
 }
